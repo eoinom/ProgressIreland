40e8bbfdc2a74cbb342ecefde1dc25756ffef487
Eoin O'Malley <eoinom@gmail.com>
2024-05-01 23:09:29 +0100

    [PI-17] Text input component

---
+++ src/components/forms/TextInput.stories.ts
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import TextInput from './TextInput.vue';
+
+const meta = {
+  title: 'Forms/Text Input',
+  component: TextInput as Record<keyof typeof TextInput, unknown>,
+  parameters: {
+    layout: 'padded',
+  },
+  tags: ['autodocs'],
+  args: {
+    type: 'text',
+    placeholder: 'Name',
+  },
+} satisfies Meta<typeof TextInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Text: Story = {
+  args: {
+    type: 'text',
+    placeholder: 'Name',
+    onInput: (event: InputEvent) => {
+      console.log('value: ', (event.target as HTMLInputElement).value);
+    },
+  },
+};
+
+export const Email: Story = {
+  args: {
+    type: 'email',
+    placeholder: 'E-mail Address',
+    onInput: (event: InputEvent) => {
+      console.log('value: ', (event.target as HTMLInputElement).value);
+    },
+  },
+};
+
+export const TextArea: Story = {
+  args: {
+    type: 'text-area',
+    placeholder: 'Message',
+    onInput: (event: InputEvent) => {
+      console.log('value: ', (event.target as HTMLInputElement).value);
+    },
+    rows: 3,
+  },
+};
---
+++ src/components/forms/TextInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <input
+    v-bind="$attrs"
+    v-model="inputValue"
+    :placeholder="props.placeholder"
+    @input="
+      $emit(
+        'update:modelValue',
+        ($event.target as HTMLInputElement)?.value ?? '',
+      )
+    "
+    class="w-full border-b border-charcoal-bright focus:outline-none placeholder-charcoal-bright placeholder:text-pi-body-3-bold text-charcoal-bright text-pi-body-3-bold"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+  type: {
+    type: String,
+    default: 'text',
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+});
+
+const inputValue = ref(props.modelValue);
+</script>
