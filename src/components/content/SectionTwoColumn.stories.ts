5d74ad5e787a121f3630694191450d5728f0d07d
Eoin O'Malley <eoinom@gmail.com>
2024-05-02 23:27:08 +0100

    [PI-23] Create SectionTwoColumn component

---
+++ src/components/content/SectionTwoColumn.stories.ts
@@ -0,0 +1,150 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import SectionTwoColumn from './SectionTwoColumn.vue';
+import Job from './Job.vue';
+
+const meta = {
+  title: 'content/SectionTwoColumn',
+  component: SectionTwoColumn as Record<keyof typeof SectionTwoColumn, unknown>,
+  parameters: {
+    layout: 'padded',
+    slots: {
+      leftColumn: {
+        description: 'Left Column Content',
+        template: `Default left slot content`,
+      },
+      rightColumn: {
+        description: 'Right Column Content',
+        template: `Default right slot content`,
+      },
+    },
+  },
+  tags: ['autodocs'],
+  // argTypes: {
+  //   bgColour: { control: 'select', options: Object.values(Colours) },
+  // },
+} satisfies Meta<typeof SectionTwoColumn>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const MissionStatement: Story = {
+  args: {
+    title: 'Mission Statement',
+  },
+  parameters: {
+    layout: 'padded',
+    slots: {
+      leftColumn: {
+        template: `
+        <h3 class="text-pi-heading-4 lg:text-pi-heading-3 text-charcoal-bright lg:max-w-[620px]">
+          Cities have long served as engines for prosperity, innovation, culture
+          and mobility.
+        </h3>
+        `,
+      },
+      rightColumn: {
+        template: `
+        <div
+          class="flex flex-col lg:flex-row lg:gap-x-pi-20 mb-pi-15 lg:mb-pi-20"
+        >
+          <div class="w-full lg:w-1/3">
+            <h4 class="text-pi-body-3-bold text-charcoal-bright">
+              Introducing the Metropolitan Abundance Project.
+            </h4>
+          </div>
+          <div class="w-full lg:w-2/3">
+            <p class="text-pi-body-3-light text-charcoal-bright">
+              A new policy center that will arm the urban wing of the abundance
+              movement with the tools necessary to create the prosperous,
+              vibrant, small-d democratic cities of the future.
+            </p>
+          </div>
+        </div>
+        <div
+          class="flex flex-col lg:flex-row lg:gap-x-pi-20 mb-pi-15 lg:mb-pi-20"
+        >
+          <div class="w-full lg:w-1/3">
+            <h4 class="text-pi-body-3-bold text-charcoal-bright">
+              We believe cities are humanity's greatest invention.
+            </h4>
+          </div>
+          <div class="w-full lg:w-2/3">
+            <p class="text-pi-body-3-light text-charcoal-bright">
+              But entrenched interests have been preventing America’s cities
+              from realizing their full potential. We aim to break down the
+              barriers that stop cities from building abundant housing, great
+              transportation systems, and responsive and effective governments.
+            </p>
+          </div>
+        </div>
+        <div class="w-full">
+          <p class="text-pi-body-3-light text-charcoal-bright">
+            Stay tuned as for our forthcoming reports, events, communications
+            tools, model legislation, and more.
+          </p>
+          <p class="text-pi-body-3-bold text-charcoal-bright">
+            Read our introductory blog post here.
+          </p>
+        </div>
+        `,
+      },
+    },
+  },
+};
+
+export const MeetOurTeam: Story = {
+  args: {
+    title: 'Our Staff',
+  },
+  parameters: {
+    layout: 'padded',
+    slots: {
+      leftColumn: {
+        template: `
+        <h3 class="text-pi-heading-4 lg:text-pi-heading-3 text-charcoal-bright mb-pi-15 lg:mb-pb-10">
+          Meet our team.
+        </h3>
+        <p class="text-pi-body-3-bold text-charcoal-bright lg:max-w-[572px]">
+          The Metropolitan Abundance Project was born at California YIMBY and is expanding nationwide. Scroll down for career opportunities!
+        </p>
+        `,
+      },
+      rightColumn: {
+        components: { Job },
+        template: `
+        <Job title="Head of Science & Innovation Research" :attributes="['Dublin', 'Hybrid', 'Full Time']" />
+        <Job title="Housing Research Associate" :attributes="['Dublin', 'Remote only', 'Full Time']" />
+        <Job title="Research Fellow" :attributes="['Dublin', 'Hybrid', 'Full Time']" />
+        <Job title="Office Manager" :attributes="['Dublin', 'Office based', 'Full Time']" />
+        `,
+      },
+    },
+  },
+};
+
+export const Opportunities: Story = {
+  args: {
+    title: 'Opportunities',
+  },
+  parameters: {
+    layout: 'padded',
+    slots: {
+      leftColumn: {
+        template: `
+        <h3 class="text-pi-heading-4 lg:text-pi-heading-3 text-charcoal-bright lg:max-w-[572px]">
+          Help abundance advocates succeed nationwide.
+        </h3>
+        `,
+      },
+      rightColumn: {
+        components: { Job },
+        template: `
+        <Job title="Head of Science & Innovation Research" :attributes="['Dublin', 'Hybrid', 'Full Time']" />
+        <Job title="Housing Research Associate" :attributes="['Dublin', 'Remote only', 'Full Time']" />
+        <Job title="Research Fellow" :attributes="['Dublin', 'Hybrid', 'Full Time']" />
+        <Job title="Office Manager" :attributes="['Dublin', 'Office based', 'Full Time']" />
+        `,
+      },
+    },
+  },
+};
---
+++ src/components/content/SectionTwoColumn.vue
@@ -0,0 +1,35 @@
+<template>
+  <section
+    class="flex flex-col bg-charcoal-lightest p-pi-10 p-pb-50 lg:pt-pi-100 lg:px-pi-20"
+  >
+    <div
+      class="w-full border-b border-charcoal-bright pb-pi-5 mb-pi-5 lg:mb-pi-20"
+    >
+      <h2
+        class="text-pi-body-5-bold lg:text-pi-body-3-bold text-charcoal-bright"
+      >
+        {{ title }}
+      </h2>
+    </div>
+
+    <div
+      class="flex flex-col lg:flex-row w-full lg:gap-x-pi-20 justify-between"
+    >
+      <div class="w-full lg:w-1/2 mb-pi-15 lg:mb-0">
+        <slot name="leftColumn"></slot>
+      </div>
+      <div class="w-full lg:w-1/2 lg:gap-x-pi-20">
+        <slot name="rightColumn"></slot>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
