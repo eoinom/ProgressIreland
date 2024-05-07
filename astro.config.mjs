6c8a5fcd7726a168455679e3cd8ffd4c76549f28
Eoin O'Malley <eoinom@gmail.com>
2024-05-08 00:29:05 +0100

    [PI-27] Switch to server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  output: 'hybrid',
+  output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
