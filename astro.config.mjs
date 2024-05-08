0ca111902305616a33ac3d2015aa70b11a083326
Eoin O'Malley <eoinom@gmail.com>
2024-05-08 01:08:24 +0100

    [PI-27] Revert to using astro node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   server: { port: 8080 },
 });
