e2356ca168f32cabeb520ea3c6e7540fbde97180
Eoin O'Malley <eoinom@gmail.com>
2024-05-13 21:59:54 +0100

    [PI-31] Install Partytown and add Google Analytics script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [
+    tailwind(),
+    vue(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   output: 'static',
   server: { port: 8080 },
 });
